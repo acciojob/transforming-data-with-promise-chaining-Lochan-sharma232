c275904ddfab731803690c8bb11bcddb93aef416
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-21 22:33:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let user1=document.getElementById("ip");
+let user2=document.getElementById("btn");
+let user3=document.getElementById("output");
+
+btn.addEventListener("onclick",function({
+	
+}))
