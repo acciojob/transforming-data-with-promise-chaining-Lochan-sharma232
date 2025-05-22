b36a10fdb3ba4e63957f58b73b65d8211b516788
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-22 14:29:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
-//your JS code here. If required.
-let user1=document.getElementById("ip");
-let user2=document.getElementById("btn");
-let user3=document.getElementById("output");
-
-btn.addEventListener("onclick",function(e){
-	e.preventDefault();
-	let myPromise=new Promise(function (resolve,reject){
-		setTimeout(function() {
-			output.textContent=`Result: ${number}`;
-			resolve(number);
+document.getElementById("btn").addEventListener("click",function(e){
+const input=parseInt(document.getElementById("ip").value);
+if(isNaN(input)){
+    alert("Please enter a valid number");
+    return;
+}
+processInput(input)
+.then(result=>{
+    document.getElementById("output").innerHTML=`Result: ${result}`;
+})
+});
+function processInput(input){
+let myPromise=new Promise(function (resolve){
+		setTimeout(()=> {
+			console.log(`Result:${input}`);
+			resolve(input);
 		},2000)//2second delay
 	})
+    .then((value)=>{
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                let result=value-2;
+                console.log(`Result:${result}`);
+                resolve(result);
+            },1000);
+        })
+    })
+       .then((value)=>{
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                let result=value-3;
+                console.log(`Result:${result}`);
+           resolve(result);
+            },2000)
+       });
+      })
+.then((value)=>{
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            let result=value/2;
+            console.log(`Result:${result}`);
+            resolve(result);
+        },2000)
+    });
 })
+.then((value)=>{
+   return new Promise(resolve=>{
+    setTimeout(()=>{
+        let result=value+10;
+        console.log(`Final Result: ${result}`);
+    resolve(result);
+    },1000)
+   });
+});
+}
+processInput(10);
